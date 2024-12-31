15d06bf677bc9bb89fe3bbc814c04d5a851339df
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2024-12-31 12:55:35 +0530

    add contact route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def mainPage():
     return  render_template("index.html")
 
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    return  render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.run(host= "0.0.0.0", port=9002, debug=True )
