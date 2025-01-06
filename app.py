c8366166fcbad01bb855e3792432c2f835034ed3
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-06 13:16:46 +0530

    removed post from / route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["GET"])
 def mainPage():
     return  render_template("index.html")
 
