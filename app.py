b14c08cdd9cb6f07b2ab3d0ef2a3f96aa24a1066
Ranjeet Kumar Yadav <stn.ranjeetyadav@gmail.com>
2025-01-12 06:02:06 +0530

    mongodb change: username and password

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 '''
-connection_string = "mongodb+srv://mongouser:mypassword@cluster0.l6iy87kj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+connection_string = "mongodb+srv://user:password@cluster0.l6iy87kj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(connection_string)
 
 # Access Database
